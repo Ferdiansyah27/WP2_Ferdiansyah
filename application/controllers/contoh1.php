055554799e88b783d6e19e5a9121a11d30d3e508
Ferdiansyah27 <kingferdi2704@gmail.com>
2022-03-21 16:18:35 +0700

    menambahkan isi contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama Saya Ferdiansyah
+        Saya Tinggal Di Cengkareng Hobi Saya Swimming";
+        
+    }
+}
